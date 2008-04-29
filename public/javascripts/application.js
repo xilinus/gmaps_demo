0882e9b097f6fb313a7915179dce4cdd425324ba
Sébastien Gruhier <sgruhier@mac.com>
2008-04-29 17:22:48 +0200

    add map action and display users on google map

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,19 +1,32 @@
 Application = new Class.create({
-  initialize: function(markers) {
-    this.markers = markers.evalJSON();
+  initialize: function(map, users) {
+    this.element = $(map);
+    this.users   = users.evalJSON();
     document.observe("dom:loaded", this.init.bind(this));
   },
   
   init: function() {
     if (GBrowserIsCompatible()) {
       // Create a google map
-      this.map = new GMap2($("map"));
+      this.map = new GMap2(this.element);
+      
       // Center on France
       this.map.setCenter(new GLatLng(46.227638, 2.213749), 6);
       
-      if (this.markers) {
-        this.markers.each(function(marker) {
-          this.map.addOverlay(new GMarker(new GLatLng(marker.lat, marker.lng)));
+      // Add controls
+      this.map.addControl(new GLargeMapControl());
+      this.map.addControl(new GMapTypeControl());
+      
+      // Add Markers
+      if (this.users) {
+        this.users.each(function(user) {
+          var marker = new GMarker(new GLatLng(user.lat, user.lng));
+          
+          GEvent.addListener(marker, "click", function() {
+            marker.openInfoWindowHtml(user.login);
+          });
+          
+          this.map.addOverlay(marker);
         }.bind(this))
       }
     }
